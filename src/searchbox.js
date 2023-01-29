e809145e1fab982be03f46c42e4b0b8ec4270eac
Gauty0025 <84655055+Gauty0025@users.noreply.github.com>
2023-01-29 15:34:17 +0530

    Update searchbox.js
    Created the pagination feature

--- src/searchbox.js
+++ src/searchbox.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect, useRef } from "react";
-import { TextField } from "@material-ui/core";
+import { TextField, Button } from "@material-ui/core";
 import SearchIcon from "@mui/icons-material/Search";
 import { InputAdornment } from "@material-ui/core";
-import { useHistory, withRouter } from "react-router-dom";
-import Animelist from "../animelist";
+import { useHistory } from "react-router-dom";
+import Animelist from "./animelist";
 
 function SearchPage() {
   const [animeData, setAnimeData] = useState([]);
   const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [pagination, setPagination] = useState({});
 
   const history = useHistory();
 
@@ -27,14 +29,29 @@ function SearchPage() {
         requestCounterRef.current = 0;
       }
       const res = await fetch(
-        `https://api.jikan.moe/v4/characters?page=0&limit=15&q=${query}&order_by=favorites&sort=desc`
+        `https://api.jikan.moe/v4/characters?page=${page}&limit=15&q=${query}&order_by=favorites&sort=desc`
       );
       const resData = await res.json();
       setAnimeData(resData.data);
+      setPagination(resData.pagination);
+      console.log(resData)
     };
     getData();
     console.log(animeData);
-  }, [query]);
+  }, [query, page]);
+
+  const handleNext = () => {
+    if (
+      
+      pagination.has_next_page &&
+      page < pagination.last_visible_page
+    )
+      setPage(page + 1);
+  };
+
+  const handlePrevious = () => {
+    if (page > 1) setPage(page - 1);
+  };
 
   return (
     <>
@@ -52,6 +69,12 @@ function SearchPage() {
       />
 
       <Animelist animeData={animeData} handleClick={handleClick} />
+      <Button onClick={handlePrevious} disabled={page === 1}>
+        Previous
+      </Button>
+      {pagination ? (
+        <Button onClick={handleNext} disabled={pagination.has_next_page && page === pagination.last_visible_page}>Next</Button>
+      ) : null}
     </>
   );
 }
